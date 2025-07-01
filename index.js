97e839537a40454ee8dd6763f30db963e9fa1d05
hukaiems <nguoinhen0105@gmail.com>
2025-07-01 12:34:43 +0700

    finish the call public api practice project

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+// tell express to handle form bodies
+app.use(express.urlencoded({extended: true}));
+
+let categories = [];
+axios.get(`https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`).then(res => {
+    categories = res.data.drinks;
+});
+
+
+app.get('/', async (req, res)=> {
+    try{
+        categories = response.data.drinks;
+        // console.log(categories);
+        res.render('index.ejs', {categories, drinks: []});
+    } catch(error){
+        console.log(error.message);
+        res.render('index.ejs', {categories, drinks: []});
+    }
+});
+
+app.post('/choose', async (req, res)=> {
+    try{
+        const category = req.body.chosenCategory;
+        // remember the https://
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
+        const drinks = response.data.drinks;
+        // console.log(JSON.stringify(response.data.drinks[0]));
+        res.render('index', {categories, drinks});
+    } catch(error){
+        console.log("Error", error.message);
+    }
+});
+
+app.listen(port, ()=> {
+    console.log(`Listening in port: ${port}`);
+});
